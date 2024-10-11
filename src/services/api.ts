8e460d132400f9f41e43a82e7f2b3a946b84c736
Yurleiszg <yurleiszuluaga.g@hotmail.com>
2024-10-11 14:00:47 -0500

    ♻️ Refactor API and Auth service files

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,41 @@
-//tood
+const baseUrl = 'http://localhost:8000/api/';
+
+const api = {
+    get: async (url: string) => {
+        const response = await fetch(baseUrl+url);
+        if (!response.ok) {
+            throw new Error('Failed to fetch data, error: ' + response.statusText);
+        }
+        return response.json();
+    },
+
+    post: async (url: string, data: null) => {
+        const response = await fetch(baseUrl+url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            throw new Error('Failed to post data, error: ' + response.statusText);
+        }
+        return response.json();
+    },
+
+    auth_get: async (url: string) => {
+
+        const token = localStorage.getItem('token');
+        if (!token) {
+            throw new Error('No token found');
+        }
+
+        const response = await fetch(baseUrl+url, {headers: {'Authorization': 'Bearer ' + token}});
+        if (!response.ok) {
+            throw new Error('Failed to fetch data, error: ' + response.statusText);
+        }
+        return response.json();
+    }
+}
+
+export default api;
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,1 +1,39 @@
-//todo
+import api from './api';
+
+interface UserRegister {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface UserLogin {
+    email: string;
+    password: string;
+}
+
+async function register(user: UserRegister):Promise<boolean> {
+    try {
+        await api.post('public/auth/register', user);
+    } catch {
+        return false;
+    }
+    return true;
+}
+
+async function login(user: UserLogin):Promise<boolean> {
+    try {
+        const server_response = await api.post('public/auth/login', user);
+        console.log(server_response.token);
+        if (server_response.token) {
+            localStorage.setItem('token', server_response.token);
+            return true;
+        }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+        return false;
+    }
+    return true;
+}
+
+export { register, login };
