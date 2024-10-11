d80bc549426fba81efb66187d1e6a4d0ce5f3a9f
AndrxsQ <andresquintana2004@gmail.com>
2024-10-11 18:06:27 -0500

    Adding login (not complete)

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export const login = () => {
+export const Login = () => {
   return (
-    <div>login</div>
+    <div>
+        <ul>
+            <li>Input</li>
+            <li>Input</li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
   )
 }
