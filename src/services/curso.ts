9110daff8a868713b3e5f68e6ef514aa4fc31e42
Yurleiszg <yurleiszuluaga.g@hotmail.com>
2024-10-12 03:22:36 -0500

    ✨ Add class and curso services

--- src/services/curso.ts
+++ src/services/curso.ts
@@ -1,9 +1,9 @@
 import api from "./api";
 
 
-const getClass = async (): Promise<string[]> => {
+const getCursos = async (): Promise<string[]> => {
     try {
-        const response = await api.auth_get('/clases');
+        const response = await api.auth_get('/cursos');
         return response.data; 
     } catch (error) {
         console.error("Error fetching teachers:", error);
@@ -11,4 +11,4 @@ const getClass = async (): Promise<string[]> => {
     }
 };
 
-export { getClass};
+export { getCursos };
