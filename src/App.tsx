ba7356c4dc75206645cc7b740f2d3ed6c9d3f4a2
Yurleiszg <yurleiszuluaga.g@hotmail.com>
2024-10-12 10:04:32 -0500

    ♻️ Refactor App component to remove unused code and clean up comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { useState } from 'react';
 import './App.css';
-import Login from './components/login';
-import Register from './components/register';
 import Home from './pages/home/Home';
 import SearchPage from './pages/search/Search';
 import Profile from './pages/profile/Profile';
 
-import TeacherRegister from './components/techerRegister'; // Corrige el nombre del archivo
 
 function App() {
-  const [currentComponent, setCurrentComponent] = useState<string>('login'); // Estado para controlar el componente a mostrar
+ // Estado para controlar el componente a mostrar
 
   return (
     <>
-    {/* <Home></Home> */}
+    <Home></Home> 
     <Profile></Profile>
-    {/* <SearchPage></SearchPage> */}
+    <SearchPage></SearchPage>
     
       {/* <div>
         <button onClick={() => setIsLogin(true)}>Login</button>
@@ -25,18 +21,18 @@ function App() {
       {/* Renderiza el componente según el estado */}
       {/* {isLogin ? <Login /> : <Register />} */}
 
-      <div>
-        {/* Botones para alternar entre Login, Register y Teacher Register */}
+      {/*<div>
+        {/* Botones para alternar entre Login, Register y Teacher Register 
         <button onClick={() => setCurrentComponent('login')}>Login</button>
         <button onClick={() => setCurrentComponent('register')}>Register</button>
         <button onClick={() => setCurrentComponent('teacherRegister')}>Teacher Register</button>
-      </div>
+      </div>*/}
 
-      {/* Renderiza el componente según el estado */}
+      {/* Renderiza el componente según el estado }
       {currentComponent === 'login' && <Login />}
       {currentComponent === 'register' && <Register />}
-      {currentComponent === 'teacherRegister' && <TeacherRegister />}
-    </>
+      {currentComponent === 'teacherRegister' && <TeacherRegister />}*/}
+    </> 
   );
 }
 
