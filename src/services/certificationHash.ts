da49598f55ad621234eadd83aca8fb78700f0442
AndrxsQ <andresquintana2004@gmail.com>
2024-10-12 09:49:36 -0500

    Modifying certificationHash endpoint

--- src/services/certificationHash.ts
+++ src/services/certificationHash.ts
@@ -2,7 +2,7 @@ import api from "./api";
 
 const getCertificationHash = async (certification_hash: string): Promise<any> => {
     try {
-        const response = await api.auth_get(`/certificar/${certification_hash}`);
+        const response = await api.get(`/certificar/${certification_hash}`);
         return response.data; 
     } catch (error) {
         console.error("Error fetching certification:", error);
